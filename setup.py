bb6ce1dade2791eb3a1fd5e06ddca60892122a56
varun <varundadhich96@gmail.com>
2025-03-04 11:58:50 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function returns the list of Requirements
+    '''
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+
+
+
+
+
+setup(
+
+    name='mlproject',
+    version='0.0.1',
+    author='Varun',
+    author_email='varundadhich96@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+
+
+
+)
